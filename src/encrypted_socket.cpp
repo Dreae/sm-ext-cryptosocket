ea34ed47e0eeeda8f11544555e89ffc1485d6c42
Dreae <dreae@dreae.onl>
2020-05-12 00:36:15 -0400

    Checkpoint

--- src/encrypted_socket.cpp
+++ src/encrypted_socket.cpp
@@ -2,14 +2,14 @@
 
 encrypted_socket::encrypted_socket(string key) {
     this->service = make_unique<boost::asio::io_service>();
-    this->socket = make_unique<tcp::socket>(this->service);
-    this->work = make_unique<boost::asio::io_service::work>(this->service);
+    this->socket = make_unique<tcp::socket>(*this->service);
+    this->work = make_unique<boost::asio::io_service::work>(*this->service);
     this->buffer = make_unique<boost::asio::streambuf>();
     this->connected = false;
 }
 
 void encrypted_socket::connect(function<void()> callback, string address, uint16_t port) {
-    auto resolver = make_shared<tcp::resolver>(this->service);
+    auto resolver = make_shared<tcp::resolver>(*this->service);
 
     char s_port[8];
     snprintf(s_port, sizeof(s_port), "%hu", port);
@@ -40,43 +40,28 @@ void encrypted_socket::connect(function<void()> callback, string address, uint16
 
 void encrypted_socket::start_read_packet() {
     auto self(shared_from_this());
-    boost::asio::async_read(*this->socket, *this->buffer, [](boost::system::error_code ec, size_t n_bytes) {
-        if (ec) {
-            smutils->LogMessage(myself, "Error reading packet: %s", ec.message().c_str());
-            return (size_t)0;
-        } else if (n_bytes >= 2) {
-            return (size_t)0;
-        }
-
-        return 2 - n_bytes;
-    }, [this, self](boost::system::error_code ec, size_t n_bytes) {
+    
+    boost::asio::async_read(*this->socket, *this->buffer, boost::asio::transfer_exactly(2), [this, self](boost::system::error_code ec, size_t n_bytes) {
         if (!ec) {
-            this->msg_size = boost::asio::buffer_cast<uint16_t>(this->buffer->data());
+            this->msg_size = *boost::asio::buffer_cast<const uint16_t *>(this->buffer->data());
             this->do_read();
         } else {
             smutils->LogMessage(myself, "Error reading packet: %s", ec.message().c_str());
+            this->start_read_packet();
         }
     });
 }
 
 void encrypted_socket::do_read() {
     auto self(shared_from_this());
-    boost::asio::async_read(*this->socket, *this->buffer, [this, self](boost::system::error_code ec, size_t n_bytes) {
+
+    boost::asio::async_read(*this->socket, *this->buffer, boost::asio::transfer_exactly(this->msg_size), [this, self](boost::system::error_code ec, size_t n_bytes) {
         if (!ec) {
-            if (n_bytes >= this->msg_size) {
-                return 0;
-            } else {
-                this->msg_size -= n_bytes;
-                this->do_read();
-            }
-        } else {
-            smutils->LogMessage(myself, "Error reading packet: %s", ec.message().c_str());
-        }
-    }, [this, self](boost::system::error_code ec, size_t n_bytes) {
-        if (!ec) {
-            this->start_read_packet();
+            // TODO: Stuff with data
         } else {
             smutils->LogError(myself, "Error reading packet: %s", ec.message().c_str());
         }
+
+        this->start_read_packet();
     });
 }
