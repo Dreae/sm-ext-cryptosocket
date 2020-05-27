75e65dc29d8e3122c225b0792b9e9347e155a0c4
Dreae <dreae@dreae.onl>
2020-05-27 00:32:47 -0400

    Build 32bit on windows

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsDevCmd.bat"
 
-cmake -DTARGET=windows -DCMAKE_BUILD_TYPE=Release ..
+cmake -A Win32 -DTARGET=windows -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release
